2f73097d3d8ee1377b66bb82f8256f1516a2e1d0
robertssumanis <sumanis4321@gmail.com>
2023-04-13 19:44:05 +0300

    First commit: added weapon with raycasting physics (with the help of internet). Added targets, that get destroyed when shot at.

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+
+    public static Action shootInput;
+    public static Action reloadInput;
+
+    [SerializeField] private KeyCode reloadKey;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0)) 
+        {
+            shootInput?.Invoke();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            reloadInput?.Invoke();
+        }
+    }
+
+}
